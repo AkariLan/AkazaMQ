8d987f63197179d2c2ab68f8a89cb9460fa566d1
AkariLan <850439027@qq.com>
2016-04-08 11:14:24 +0800

    create project

---
+++ src/main/java/akari/lan/akaza/App.java
@@ -0,0 +1,13 @@
+package akari.lan.akaza;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
