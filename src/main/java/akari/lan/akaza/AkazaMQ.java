171cd400fca5b2962c1c0e8256d19997b944ddc0
AkariLan <850439027@qq.com>
2016-04-08 11:29:49 +0800

    Add Message Server

---
+++ src/main/java/akari/lan/akaza/AkazaMQ.java
@@ -0,0 +1,14 @@
+package akari.lan.akaza;
+/**
+ * @author AkariLan
+ * @version 0.0.1
+ * Main entry of AkazaMQ
+ */
+public class AkazaMQ {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/main/java/akari/lan/akaza/server/MessageServer.java
@@ -0,0 +1,11 @@
+package akari.lan.akaza.server;
+
+public class MessageServer implements Runnable {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
